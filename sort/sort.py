ebd1c1c9cb476d903c3f9a56f96d0747d71ceff8
flowarko <flowarko@gmail.com>
2021-03-29 09:11:29 +0200

    - Documentation

--- sort/sort.py
+++ sort/sort.py
@@ -3,23 +3,31 @@ def sort_recursion(ext_seq):
     ext_seq: input !list with numbers
     :return: Ordered list
     '''
+    # Copy the input list, because pop() will
+    # change the original list
     sequence = ext_seq.copy()
+
+    # Check if sequence is > 1 and numeric
     if len(sequence) <= 1:
         return sequence
-    # Check if sequence is numeric
     for s in sequence:
         if not str(s).isdigit():
             raise ValueError('sequence must be all numeric')
 
+    # Initialize lists and first pivot number
     lower_number = []
     greater_number = []
     pivot = sequence.pop()
 
+    # Compare every item in sequence with pivot
     for item in sequence:
         if item < pivot:
             lower_number.append(item)
         else:
             greater_number.append(item)
+
+    # Run algorithm again and again, until
+    # length sequence is <= 1
     return sort_recursion(lower_number) + [pivot] + sort_recursion(greater_number)
 
 
@@ -28,14 +36,16 @@ def sort_insertion(sequence):
     sequence: input !list with numbers
     :return: Ordered list
     '''
+    # Check if sequence is > 1 and numeric
     if len(sequence) <= 1:
         return sequence
-    # Check if sequence is numeric
     for s in sequence:
         if not str(s).isdigit():
             raise ValueError('sequence must be all numeric')
 
     length = range(0, len(sequence))
+    # Insert every item in sequence and compare it
+    # until it is bigger than i - 1
     for i in length:
         value = sequence[i]
         while sequence[i-1] > value and i > 0:
