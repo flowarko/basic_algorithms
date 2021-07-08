204a8a4a941f327099146ad834803be48a342329
flowarko <flowarko@gmail.com>
2021-07-08 10:48:57 +0200

    - added icecream for debugging

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from wmath.prime import prime_check
 from wmath.fib import fibonacci
 from random import randint
 from wmath.temperatures import *
+from icecream import ic
 
 if __name__ == '__main__':
     # Testing Algorithms
     test_list = [randint(0, 100) for i in range(10)]
-    test_number = randint(0, 100)
+    test_number = randint(0, 20)
     test_number_celsius = randint(0, 40)
     print('Testlist: ' + str(test_list))
     print('Sort Recursion: ' + str(sort_recursion(test_list)))
@@ -16,6 +17,16 @@ if __name__ == '__main__':
     print('Lowest Number: ' + str(lnumber(test_list)))
     print('Highest Number: ' + str(hnumber(test_list)))
     print('Prime Check of {}: {}'.format(test_number, prime_check(test_number)))
-    print('Fibonacci of 10: {}'.format(fibonacci(10)))
+    print('Fibonacci of 10: {}'.format(fibonacci(test_number)))
     print('Converting {} Fahrenheit to Celsius: {}'.format(test_number, f_in_c(test_number)))
     print('Converting {} Celsius to Fahrenheit: {}'.format(test_number_celsius, c_in_f(test_number_celsius)))
+    print('')
+    # Testing icecream for debugging
+    ic(sort_recursion(test_list))
+    ic(sort_insertion(test_list))
+    ic(lnumber(test_list))
+    ic(hnumber(test_list))
+    ic(prime_check(test_number))
+    ic(fibonacci(test_number))
+    ic(f_in_c(test_number))
+    ic(c_in_f(test_number_celsius))
